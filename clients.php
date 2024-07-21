b2b5a2de3c2629563387411e4163abda29a43285
omarelkadiri <150052899+omarelkadiri@users.noreply.github.com>
2024-07-21 11:04:41 +0100

    première commit : front

---
+++ clients.php
@@ -0,0 +1,35 @@
+<?php 
+    $clients=true;
+    include_once("header.php");
+?>
+
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Clients</h1>
+
+  <table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+  </table>
+
+  </div>
+</main>
+
+<?php 
+    include_once("footer.php");
+?>
---
+++ commandes.php
@@ -0,0 +1,34 @@
+<?php 
+    $commandes=true;
+    include_once("header.php");
+?>
+
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Commandes</h1>
+  </div>
+</main>
+  
+  <table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+  </table>
+
+<?php 
+    include_once("footer.php");
+?>
